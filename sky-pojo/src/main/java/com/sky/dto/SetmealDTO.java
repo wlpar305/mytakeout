c92e03e5e33002fe63bf8e3212aec1c7a8121c1f
亮程 王 <wlpar305@163.com>
2024-11-27 15:37:11 +0800

    update redis

---
+++ sky-pojo/src/main/java/com/sky/dto/SetmealDTO.java
@@ -0,0 +1,20 @@
+package com.sky.dto;
+
+import com.sky.entity.SetmealDish;
+import lombok.Data;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class SetmealDTO implements Serializable {
+    private Long id;
+    private Long categoryId;
+    private String name;
+    private BigDecimal price;
+    private Integer status;
+    private String description;
+    private String image;
+    private List<SetmealDish> setmealDishes = new ArrayList<>();
+}
