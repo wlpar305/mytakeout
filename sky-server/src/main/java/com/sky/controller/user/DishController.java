07ffe7fa4acef4c7ac26f390d536f42b7d0dc2d7
亮程 王 <wlpar305@163.com>
2024-11-29 10:56:43 +0800

    update user dish setmeal category

---
+++ sky-server/src/main/java/com/sky/controller/user/DishController.java
@@ -0,0 +1,43 @@
+package com.sky.controller.user;
+
+import com.sky.constant.StatusConstant;
+import com.sky.dto.DishDTO;
+import com.sky.entity.Dish;
+import com.sky.result.Result;
+import com.sky.service.DishService;
+import com.sky.vo.DishVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Set;
+
+@RestController("userDishController")
+@RequestMapping("/user/dish")
+@Slf4j
+@Api(tags = "C端-菜品浏览接口")
+public class DishController {
+    @Autowired
+    private DishService dishService;
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @GetMapping("/list")
+    @ApiOperation("根据分类id查询菜品")
+    public Result<List<DishVO>> list(Long categoryId) {
+        String key = "dish_" + categoryId;
+        List<DishVO> list = (List<DishVO>) redisTemplate.opsForValue().get(key);
+        if (list != null && list.size() > 0) {
+            return Result.success(list);
+        }
+        Dish dish = new Dish();
+        dish.setCategoryId(categoryId);
+        dish.setStatus(StatusConstant.ENABLE);
+        list = dishService.listWithFlavor(dish);
+        redisTemplate.opsForValue().set(key, list);
+        return Result.success(list);
+    }
+}
