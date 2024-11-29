b4009144586cd5e73ff5911de4f1dc2f40b47a32
亮程 王 <wlpar305@163.com>
2024-11-29 19:25:58 +0800

    update order

---
+++ sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -0,0 +1,22 @@
+package com.sky.controller.user;
+
+import com.sky.dto.OrdersPaymentDTO;
+import com.sky.dto.OrdersSubmitDTO;
+import com.sky.result.PageResult;
+import com.sky.result.Result;
+import com.sky.service.OrderService;
+import com.sky.vo.OrderPaymentVO;
+import com.sky.vo.OrderSubmitVO;
+import com.sky.vo.OrderVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController("userOrderController")
+@RequestMapping("/user/order")
+@Slf4j
+@Api(tags = "C端-订单接口")
+public class OrderController {
+}
--- sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -67,10 +67,50 @@ public class OrderServiceImpl implements OrderService {
     @Autowired
     private WebSocketServer webSocketServer;
 
+    @Override
+    public OrderSubmitVO submitOrder(OrdersSubmitDTO ordersSubmitDTO) {
+        AddressBook addressBook=addressBookMapper.getById(ordersSubmitDTO.getAddressBookId());
+        if(addressBook==null){
+            throw new AddressBookBusinessException(MessageConstant.ADDRESS_BOOK_IS_NULL);
+        }
+        checkOutOfRange(addressBook.getCityName()+addressBook.getDistrictName()+addressBook.getDetail());
+
+        Long currentId=5L;
+        ShoppingCart shoppingCart=new ShoppingCart();
+        shoppingCart.setUserId(currentId);
+        List<ShoppingCart> shoppingCartList=shoppingCartMapper.list(shoppingCart);
+        if(shoppingCartList==null||shoppingCartList.size()==0){
+            throw new ShoppingCartBusinessException(MessageConstant.SHOPPING_CART_IS_NULL);
+        }
+        Orders order=new Orders();
+        BeanUtils.copyProperties(ordersSubmitDTO,order);
+        order.setPhone(addressBook.getPhone());
+        order.setAddress(addressBook.getDetail());
+        order.setConsignee(addressBook.getConsignee());
+        order.setNumber(String.valueOf(System.currentTimeMillis()));
+        order.setUserId(currentId);
+        order.setStatus(Orders.PENDING_PAYMENT);
+        order.setPayStatus(Orders.UN_PAID);
+        order.setOrderTime(LocalDateTime.now());
+        orderMapper.insert(order);
+
+        ArrayList<OrderDetail> orderDetailList=new ArrayList<>();
+        shoppingCartList.forEach(cart->{
+            OrderDetail orderDetail=new OrderDetail();
+            BeanUtils.copyProperties(cart,orderDetail);
+            orderDetail.setOrderId(order.getId());
+            orderDetailList.add(orderDetail);
+        });
+        orderDetailMapper.insertBatch(orderDetailList);
+        shoppingCartMapper.deleteByUserId(currentId);
+        OrderSubmitVO submitVO=OrderSubmitVO.builder().id(order.getId()).orderNumber(order.getNumber()).orderAmount(order.getAmount()).orderTime(order.getOrderTime()).build();
+
+        return submitVO;
+    }
 
     @Override
     public OrderPaymentVO payment(OrdersPaymentDTO ordersPaymentDTO) throws Exception {
-        Long userId = BaseContext.getCurrentId();
+        Long userId = 5L;
         User user = userMapper.getById(String.valueOf(userId));
         String orderNumber = ordersPaymentDTO.getOrderNumber();
         Orders orders = orderMapper.getByNumberAndUserId(orderNumber, userId);
@@ -84,5 +124,240 @@ public class OrderServiceImpl implements OrderService {
         OrderPaymentVO vo = jsonObject.toJavaObject(OrderPaymentVO.class);
         vo.setPackageStr(jsonObject.getString("package"));
         return vo;
+    }
+
+    @Override
+    public void paySuccess(String outTradeNo){
+        Long userId = 5L;
+        Orders orderDB = orderMapper.getByNumberAndUserId(outTradeNo, userId);
+        Orders orders = Orders.builder().id(orderDB.getId()).status(Orders.TO_BE_CONFIRMED).payStatus(Orders.PAID).checkoutTime(LocalDateTime.now()).build();
+        orderMapper.update(orders);
+        HashMap map=new HashMap();
+        map.put("type", 1);
+        map.put("orderId", orders.getId());
+        map.put("content", "订单号：" + outTradeNo);
+        webSocketServer.sendToAllClient(JSON.toJSONString(map));
+    }
+
+    @Override
+    public PageResult pageQueryForUser(int pageNum,int pageSize,Integer status){
+        PageHelper.startPage(pageNum,pageSize);
+        OrdersPageQueryDTO ordersPageQueryDTO=new OrdersPageQueryDTO();
+        ordersPageQueryDTO.setStatus(status);
+        ordersPageQueryDTO.setUserId(5L);
+        Page<Orders> page=orderMapper.pageQuery(ordersPageQueryDTO);
+        ArrayList<OrderVO> list=new ArrayList<>();
+        if(page!=null&&page.getTotal()>0){
+            page.forEach(orders->{
+                Long ordersId=orders.getId();
+                List<OrderDetail> orderDetailList = orderDetailMapper.getByOrderId(ordersId);
+                OrderVO orderVO = new OrderVO();
+                BeanUtils.copyProperties(orders, orderVO);
+                orderVO.setOrderDetailList(orderDetailList);
+                list.add(orderVO);
+            });
+        }
+        return  new PageResult(page.getTotal(),list);
+    }
+
+    @Override
+    public OrderVO details(Long id){
+        Orders orders=orderMapper.getById(id);
+        List<OrderDetail> orderDetailList=orderDetailMapper.getByOrderId(id);
+        OrderVO orderVO = new OrderVO();
+        BeanUtils.copyProperties(orders,orderVO);
+        orderVO.setOrderDetailList(orderDetailList);
+        return orderVO;
+    }
+
+    @Override
+    public void userCancelById(Long id)throws Exception{
+        Orders orderDB=orderMapper.getById(id);
+        if(orderDB==null){
+            throw new OrderBusinessException(MessageConstant.ORDER_NOT_FOUND);
+        }
+        if(orderDB.getStatus()>2){
+            throw new OrderBusinessException(MessageConstant.ORDER_STATUS_ERROR);
+        }
+        Orders orders=new Orders();
+        orders.setId(orderDB.getId());
+        if(orderDB.getStatus().equals(Orders.TO_BE_CONFIRMED)){
+            weChatPayUtil.refund(orderDB.getNumber(),orderDB.getNumber(),orders.getAmount(),orders.getAmount());
+            orders.setPayStatus(Orders.REFUND);
+        }
+        orders.setStatus(Orders.CANCELLED);
+        orders.setCancelReason("用户取消");
+        orders.setCancelTime(LocalDateTime.now());
+        orderMapper.update(orders);
+    }
+
+    @Override
+    public void repetition(Long id){
+        Long userId = 5L;
+        List<OrderDetail> orderDetailList=orderDetailMapper.getByOrderId(id);
+        List<ShoppingCart> shoppingCartList = orderDetailList.stream().map(orderDetail -> {
+            ShoppingCart shoppingCart = new ShoppingCart();
+            BeanUtils.copyProperties(orderDetail, shoppingCart, "id");
+            shoppingCart.setUserId(userId);
+            shoppingCart.setCreateTime(LocalDateTime.now());
+            return shoppingCart;
+        }).collect(Collectors.toList());
+        shoppingCartMapper.insertBatch(shoppingCartList);
+    }
+
+    @Override
+    public PageResult conditionSearch(OrdersPageQueryDTO ordersPageQueryDTO){
+        PageHelper.startPage(ordersPageQueryDTO.getPage(),ordersPageQueryDTO.getPageSize());
+        Page<Orders> pageQuery = orderMapper.pageQuery(ordersPageQueryDTO);
+        List<OrderVO> orderVOList=getOrderVoList(pageQuery);
+        return  new PageResult(pageQuery.getTotal(),orderVOList);
+    }
+
+    @Override
+    public OrderStatisticsVO statistics(){
+        Integer toBeConfirmed = orderMapper.countStatus(Orders.TO_BE_CONFIRMED);
+        Integer confirmed = orderMapper.countStatus(Orders.CONFIRMED);
+        Integer deliveryInProgress = orderMapper.countStatus(Orders.DELIVERY_IN_PROGRESS);
+        OrderStatisticsVO orderStatisticsVO = new OrderStatisticsVO();
+        orderStatisticsVO.setToBeConfirmed(toBeConfirmed);
+        orderStatisticsVO.setConfirmed(confirmed);
+        orderStatisticsVO.setDeliveryInProgress(deliveryInProgress);
+        return orderStatisticsVO;
+    }
+
+    @Override
+    public void confirm(OrdersCancelDTO ordersCancelDTO) {
+        Orders orders = Orders.builder().id(ordersCancelDTO.getId()).status(Orders.CONFIRMED).build();
+        orderMapper.update(orders);
+    }
+
+    @Override
+    public void rejection(OrdersRejectionDTO ordersRejectionDTO) throws Exception {
+        Orders ordersDB=orderMapper.getById(ordersRejectionDTO.getId());
+        if (ordersDB == null || !ordersDB.getStatus().equals(Orders.TO_BE_CONFIRMED)) {
+            throw new OrderBusinessException(MessageConstant.ORDER_STATUS_ERROR);
+        }
+        Integer payStatus=ordersDB.getPayStatus();
+        if (payStatus == Orders.PAID) {
+            String refund = weChatPayUtil.refund(ordersDB.getNumber(), ordersDB.getNumber(), ordersDB.getAmount(), ordersDB.getAmount());
+            log.info("申请退款：{}", refund);
+        }
+        Orders orders = new Orders();
+        orders.setId(ordersDB.getId());
+        orders.setStatus(Orders.CANCELLED);
+        orders.setRejectionReason(ordersRejectionDTO.getRejectionReason());
+        orders.setCancelTime(LocalDateTime.now());
+        orderMapper.update(orders);
+    }
+
+    @Override
+    public void cancel(OrdersCancelDTO ordersCancelDTO) throws Exception {
+        Orders ordersDB=orderMapper.getById(ordersCancelDTO.getId());
+        Integer payStatus=ordersDB.getPayStatus();
+        if (payStatus == 1) {
+            String refund = weChatPayUtil.refund(ordersDB.getNumber(), ordersDB.getNumber(), ordersDB.getAmount(), ordersDB.getAmount());
+            log.info("申请退款：{}", refund);
+        }
+        Orders orders = new Orders();
+        orders.setId(ordersCancelDTO.getId());
+        orders.setStatus(Orders.CANCELLED);
+        orders.setCancelReason(ordersCancelDTO.getCancelReason());
+        orders.setCancelTime(LocalDateTime.now());
+        orderMapper.update(orders);
+    }
+
+    @Override
+    public void delivery(Long id){
+        Orders orderDB=orderMapper.getById(id);
+        if (orderDB == null || !orderDB.getStatus().equals(Orders.CONFIRMED)) {
+            throw new OrderBusinessException(MessageConstant.ORDER_STATUS_ERROR);
+        }
+        Orders orders = new Orders();
+        orders.setId(orderDB.getId());
+        orders.setStatus(Orders.DELIVERY_IN_PROGRESS);
+        orderMapper.update(orders);
+    }
+
+    @Override
+    public void complete(Long id) {
+        Orders orderDB = orderMapper.getById(id);
+        if (orderDB == null || !orderDB.getStatus().equals(Orders.DELIVERY_IN_PROGRESS)) {
+            throw new OrderBusinessException(MessageConstant.ORDER_STATUS_ERROR);
+        }
+        Orders orders = new Orders();
+        orders.setId(orders.getId());
+        orders.setStatus(Orders.CONFIRMED);
+        orders.setDeliveryTime(LocalDateTime.now());
+        orderMapper.update(orders);
+    }
+
+    @Override
+    public void reminder(Long id) {
+        Orders orders = orderMapper.getById(id);
+        if (orders == null) {
+            throw new OrderBusinessException(MessageConstant.ORDER_NOT_FOUND);
+        }
+        HashMap map = new HashMap();
+        map.put("type", 2);
+        map.put("orderId", id);
+        map.put("content", "订单号：" + orders.getNumber());
+        webSocketServer.sendToAllClient(JSON.toJSONString(map));
+    }
+
+    private List<OrderVO> getOrderVoList(Page<Orders> page) {
+        ArrayList<OrderVO> orderVOArrayList = new ArrayList<>();
+        List<Orders> ordersList = page.getResult();
+        if (!CollectionUtils.isEmpty(ordersList)) {
+            ordersList.forEach(orders -> {
+                OrderVO orderVO = new OrderVO();
+                BeanUtils.copyProperties(orders, orderVO);
+                String orderDishStr = getOrderDishStr(orders);
+                orderVO.setOrderDishes(orderDishStr);
+                orderVOArrayList.add(orderVO);
+            });
+        }
+        return orderVOArrayList;
+    }
+
+    private String getOrderDishStr(Orders orders) {
+        List<OrderDetail> orderDetailList = orderDetailMapper.getByOrderId(orders.getId());
+        List<String> ordewrDishList = orderDetailList.stream().map(orderDetail -> orderDetail.getName() + "*" + orderDetail.getNumber() + ";").collect(Collectors.toList());
+        return String.join("", ordewrDishList);
+    }
+
+    private void checkOutOfRange(String address) {
+        HashMap map = new HashMap();
+        map.put("address", shopAddress);
+        map.put("output","json");
+        map.put("ak",ak);
+        String shopCoordinate = HttpClientUtil.doGet("https://api.map.baidu.com/geocoding/v3", map);
+        JSONObject jsonObject = JSON.parseObject(shopCoordinate);
+        if(!jsonObject.getString("status").equals("0")){
+            throw new OrderBusinessException("地址解析失败");
+        }
+        JSONObject location=jsonObject.getJSONObject("result").getJSONObject("location");
+        String lat=location.getString("lat");
+        String lng=location.getString("lng");
+        String shopLngLat=lat+","+lng;
+        map.put("address",address);
+        String userCoordinate=HttpClientUtil.doGet("https://api.map.baidu.com/geocoding/v3", map);
+        location = JSON.parseObject("result").getJSONObject("location");
+        lat = location.getString("lat");
+        lng = location.getString("lng");
+        String userLngLat = lat + "," + lng;
+        map.put("orgin", shopLngLat);
+        map.put("destination", userLngLat);
+        map.put("steps_info", "0");
+        String json = HttpClientUtil.doGet("https://api.map.baidu.com/directionlite/v1/driving", map);
+        jsonObject = JSON.parseObject(json);
+        if (!jsonObject.getString("status").equals("0")) {
+            throw new OrderBusinessException("配送线路规划失败");
+        }
+        JSONObject result = jsonObject.getJSONObject("result");
+        JSONArray jsonArray = (JSONArray) result.get("routes");
+        Integer distance = (Integer) ((JSONObject) jsonArray.get(0)).get("distance");
+        if(distance>5000){
+            throw new OrderBusinessException("超出配送范围");
+        }
     }
 }
